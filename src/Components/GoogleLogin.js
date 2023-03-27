4d86a4f1bdc76d413ac9f6d08b5b954f11ca392a
Abhaykumar(Iteanz) <iteanzabhaykumar@titan.co.in>
2023-03-27 17:21:52 +0530

    loginPage

--- src/Components/GoogleLogin.js
+++ src/Components/GoogleLogin.js
@@ -1,15 +1,20 @@
 /*global google*/
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "../Style/Login.css";
 import jwt_decode from "jwt-decode";
+import { FaLock } from "react-icons/fa";
 import "../Style/index.css";
 import { useNavigate } from "react-router-dom";
+import { Field, Form, Formik } from "formik";
+import { LoginInitialValue, LoginSchema } from "../Schema/LoginSchema";
+import ShowError from "../Schema/ShowError";
 
 export default function Login() {
   const [user, setUser] = useState("");
-  // const [btqId, setBtqId] = useState("");
-  // const [region, setRegion] = useState("");
   const [availableCount, setAvailableCount] = useState("");
+  const [passwordShown, setPasswordShown] = useState(false);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   function handleCallbackResponse(response) {
     var userObject = jwt_decode(response.credential);
@@ -30,12 +35,15 @@ export default function Login() {
     });
   }, []);
 
-  useEffect(() => {
+  const onLogin = (payload) => {
+    const { username, password, rsoName } = payload;
+    console.log(username, password, rsoName);
     axios
       .get(
         `https://tanishqdigitalnpim.titan.in:8443/bottomUp/BottomUp//getRegion/${user.email}`
       )
       .then((response) => {
+        setLoading(true);
         console.log("response==>", response.data);
         setAvailableCount(response.data.value.availableCount);
         localStorage.setItem("btqId", response.data.value.btqCode);
@@ -43,15 +51,19 @@ export default function Login() {
         if (response.data.status === true) {
           navigate("/bottom/up/feedback/form");
         }
+        setLoading(false);
       })
       .catch((error) => console.log("error=>", error));
-  }, [user.email, navigate]);
+  };
 
   console.log("availableCount==>", availableCount);
 
+  const togglePassword = () => {
+    setPasswordShown(!passwordShown);
+  };
   return (
     <>
-      <center>
+      {/* <center>
         <header className="LoginPage shadow">
           <br />
           <h2>Welcome to Bottom UP</h2>
@@ -60,7 +72,82 @@ export default function Login() {
           <h4 className="my-3">Continue with Your Email</h4>
           <button id="signInDiv" className="LoginButton" />
         </header>
-      </center>
+      </center> */}
+      <div className="row mx-0">
+        <div className="col RegisterLeftStyle">
+          <h5 className="text-center my-5 text-info">LOGIN WITH US</h5>
+        </div>
+        <div className="col RegisterLeftRight">
+          <div className="text-center text-info my-4">
+            <FaLock size={30} />
+            <h4>LOGIN</h4>
+          </div>
+          <Formik
+            initialValues={LoginInitialValue}
+            validationSchema={LoginSchema}
+            onSubmit={(payload) => onLogin(payload)}
+          >
+            <Form>
+              <div className="my-2">
+                <b>
+                  Username <span className="text-danger"> *</span>
+                </b>
+                <Field
+                  placeholder="Username"
+                  name="username"
+                  className="GInput"
+                />
+                <ShowError name="username" />
+              </div>
+              <div className="my-2">
+                <b>
+                  Password <span className="text-danger"> *</span>
+                </b>
+                <Field
+                  type={passwordShown ? "text" : "password"}
+                  placeholder="Password"
+                  className="GInput"
+                  name="password"
+                />
+                <ShowError name="password" />
+              </div>
+              <div className="my-2">
+                <b>
+                  RSO Name <span className="text-danger"> *</span>
+                </b>
+                <Field
+                  placeholder="RSO Name"
+                  className="GInput"
+                  name="rsoName"
+                />
+                <ShowError name="rsoName" />
+              </div>
+              <div className="d-flex justify-content-between mx-2">
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    onClick={togglePassword}
+                  />
+                  <span className="text-info">Show Password</span>
+                </div>
+                <button type="submit" className="CButton">
+                  {loading ? (
+                    <span
+                      className="spinner-border spinner-border-sm"
+                      role="status"
+                      aria-hidden="true"
+                    />
+                  ) : (
+                    <span className="sr-only">LOGIN</span>
+                  )}
+                </button>
+              </div>
+            </Form>
+          </Formik>
+          <hr className="mx-4" />
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/Schema/LoginSchema.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-useless-escape */
+import * as yup from "yup";
+
+export const LoginInitialValue = {
+  username: "",
+  password: "",
+  rsoName: "",
+};
+
+export const LoginSchema = yup.object({
+  username: yup.string().required("Username is required"),
+  password: yup
+    .string()
+    .required("Password is required")
+    .matches(
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/g,
+      "Password should be strong"
+    )
+    .min(8, "Minimum 8 character"),
+  rsoName: yup.string().required("RSO Name Is required"),
+});
